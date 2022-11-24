720210159bbca71d333f50d83f19d7152e92a84d
Мария Щербакова <isip_m.a.sherbakova@mpt.ru>
2022-11-24 23:43:50 +0300

    Практическая работа №3 first commit

--- src/main/java/com/example/pr_spring_1/Controllers/StarController.java
+++ src/main/java/com/example/pr_spring_1/Controllers/StarController.java
@@ -5,10 +5,10 @@ import com.example.pr_spring_1.Repository.StarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/star")
@@ -27,8 +27,49 @@ public class StarController {
             @RequestParam(name="lumen") int lumen){
         Star new_star = new Star(name, class_star, lumen);
         starRepository.save(new_star);
-        return "star/star-add";}
+        return "redirect:/star/";}
     @GetMapping("/star-add/")
     public String AddView(){
         return "star/star-add";}
+    @GetMapping("/filter/")
+    public String filter(
+            @RequestParam(name="name") String name,
+            Model model){
+        List<Star> starList = starRepository.findByName(name);
+        model.addAttribute("star_list", starList);
+        return"star/index";
+    }
+    @GetMapping("/filtercontains/")
+    public String filterContains(
+            @RequestParam(name="name") String name,
+            Model model){
+        List<Star> starList = starRepository.findByNameContains(name);
+        model.addAttribute("star_list", starList);
+        return"star/index";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detailStar(
+            @PathVariable Long id,
+            Model model
+    ){
+        Star star_obj = starRepository.findById(id).orElseThrow();
+        model.addAttribute("one_star", star_obj);
+        return "star/info";
+    }
+    @PostMapping ("/detail/{id}/del")
+    public String  delStar(@PathVariable Long id)
+    {
+        Star star_obj = starRepository.findById(id).orElseThrow();
+        starRepository.delete(star_obj);
+        //==
+        //starRepository.deleteById(id);
+        return "redirect:/star/";
+    }
+    @PostMapping ("/detail/{id}/upd")
+    public String  updateView(@PathVariable Long id,Model model)
+    {
+        model.addAttribute("object",starRepository.findById(id).orElseThrow());
+        return "star/update";
+    }
 }
--- src/main/java/com/example/pr_spring_1/Repository/StarRepository.java
+++ src/main/java/com/example/pr_spring_1/Repository/StarRepository.java
@@ -1,8 +1,16 @@
 package com.example.pr_spring_1.Repository;
 
 import com.example.pr_spring_1.Models.Star;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StarRepository extends CrudRepository<Star, Long> {
+
+    public List<Star> findByName(String name);
+    public List<Star> findByNameContains(String name);
+    @Query(value="SELECT * FROM star WHERE class_star =?1",nativeQuery=true)
+    List<Star> selectStars(String class_star);
 
 }
